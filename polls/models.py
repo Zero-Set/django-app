1b31c64b84016ced464d76fcebe990d945e5904c
Zero-Set <stonickers@gmail.com>
2021-03-09 23:48:00 +0900

    Add Django tutorial 3-7

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -9,9 +12,13 @@ class Question(models.Model):
 	# オブジェクトの表現
 	def __str__(self):
 		return self.question_text
-	# タイムゾーン関連
+	# タイムゾーン関連 
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
@@ -22,4 +29,4 @@ class Choice(models.Model):
 	votes = models.IntegerField(default=0)
 	# オブジェクトの表現
 	def __str__(self):
-		return self.question_text
+		return self.choice_text
